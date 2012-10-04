94fb8fd01986816f382af42f7e19043b83f012e5
galtsin <galtsinak@lad.local>
2012-10-04 17:20:24 +0400

    refactor

--- public/theme/default/js/core/sandbox/Screen.js
+++ public/theme/default/js/core/sandbox/Screen.js
@@ -1,24 +1,49 @@
 /* Менеджер Экранов */
 dojo.provide("core.sandbox.Screen");
-require(["dojo/store/Memory", "core/sandbox/screen/Breadcrumb"], function(Memory, Breadcrumb){
+require(["dojo/hash", "dojo/store/Memory", "core/sandbox/screen/Breadcrumb"], function(Hash, Memory, Breadcrumb){
     core.sandbox.Screen = function(){};
     dojo.declare("core.sandbox.Screen", null, {
         constructor: function(){
             this._screen = new Memory({data:[]});
             this.Breadcrumb = new Breadcrumb(this);
+            this._screensIndex = [];
+            this._screens = [];
             return this;
         },
-        /* добавить экран*/
-        /*
-        * Экраны используются для загрузки одной законченной части страницы
-        * Screen.add(screenName, functionName);
-        */
-        add: function(name, callback){
-            this._screen.add({name: name, callback: callback});
+        _getScreenIndex: function(identity){
+            for(var i = 0; i < this._screensIndex.length; i ++) {
+                if(identity === this._screensIndex[i]){
+                    return i;
+                }
+            }
+            return undefined;
         },
-        /* получить экран */
-        get: function(name){
-            return this._screen.query({name: name})[0];
+        add: function(identity, func){
+            if(this._getScreenIndex(identity) === undefined){
+                this._screens.push(func);
+                this._screensIndex.push(identity);
+            } else {
+                this._screens[this._getScreenIndex(identity)] = func;
+            }
+        },
+        invoke: function(identity, hash, callback){
+            //if(null !== hash){
+                dojo.hash(hash);
+            //}
+            callback(this._screens[this._getScreenIndex(identity)]);
+        },
+        // #!/lineBoars/12
+        parseHash: function(hash){
+            // Удаление #
+            var position = hash.indexOf('#');
+            var parts = (position == -1) ? hash.split('/') : hash.slice(position + 1).split('/');
+            return {
+                prefix: parts.shift(),
+                parts: parts
+            }
+        },
+        hash: function(hash){
+
         }
     });
 });
