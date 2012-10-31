a14ae5967308b94c046d86479e6e82db743f4932
galtsin <galtsinak@lad.local>
2012-10-31 17:19:50 +0400

    Possibility

---
+++ public/theme/default/js/core/helper/form.js
@@ -0,0 +1,21 @@
+define([], function(){
+    return {
+        getSelectedOptions: function(selectbox){
+            var result = [];
+            var option = null;
+            for(var i = 0, len = selectbox.options.length; i < len; i ++) {
+                option = selectbox.options[i];
+                if(option.selected) {
+                    result.push(option);
+                }
+            }
+            return result;
+        },
+        moveSelectedOptions: function(selectboxSource, selectboxRecipient){
+            var options = this.getSelectedOptions(selectboxSource);
+            for(var i = 0, len = options.length; i < len; i ++ ) {
+                selectboxRecipient.appendChild(selectboxSource.removeChild(options[i]));
+            }
+        }
+    };
+});
