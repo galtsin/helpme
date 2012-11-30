0027d77a91aedbfab8e647b7de5d3842d9d9ed53
galtsin <galtsinak@lad.local>
2012-11-30 16:30:33 +0400

    Рефакторинг кода

--- app/modules/default/controllers/ErrorController.php
+++ app/modules/default/controllers/ErrorController.php
@@ -1,30 +1,41 @@
 <?php
 
-class Default_ErrorController extends Zend_Controller_Action
+class Default_ErrorController extends App_Zend_Controller_Action
 {
 
-    /**
-     * TODO: Обработать AJAX запросы
-     */
+    public function init()
+    {
+        // Поддерживается только один контекст JSON
+        $this->getHelper('AjaxContext')
+            ->addActionContext($this->getRequest()->getActionName(), array('json', 'html'))
+            ->setAutoDisableLayout(true)
+            ->setSuffix('html', null, true) // Отключить суффикс вида
+            ->initContext();
+
+        if(null === $this->getHelper('AjaxContext')->getCurrentContext()){
+            $this->_helper->layout->setLayout('empty');
+        }
+    }
+
     public function errorAction()
     {
+        // Если получаем AJAX-запрос, то отключаем Layout и выводим то что есть
+
         $errors = $this->_getParam('error_handler');
-        
+
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
-                $this->_helper->layout->setLayout('empty');
-                $this->_helper->viewRenderer("404");
-                $this->view->message = 'Page not found';
+                $this->_helper->viewRenderer('404');
+                $this->view->message = 'Resource not found';
                 //$this->getResponse()->setRawHeader('HTTP/1.1 404 Not Found');
                 break;
             case App_Zend_Controller_Plugin_Access::EXCEPTION_ACCESS_DENIED:
                 $this->getResponse()->setHttpResponseCode(403);
-                $this->_helper->layout->setLayout('empty');
-                $this->_helper->viewRenderer("403");
+                $this->_helper->viewRenderer('403');
                 $this->view->message = 'Forbidden';
                 $this->_helper->getHelper('AjaxContext')->initContext('json');
                 break;
